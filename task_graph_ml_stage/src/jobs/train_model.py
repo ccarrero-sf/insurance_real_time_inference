6bde1400ed0105e9fd3c3596b34600df7df604f1
Carlos Carrero <carlos.carrero@snowflake.com>
2026-03-12 13:51:05 +0100

    2nd commit test WIP

--- task_graph_ml_stage/src/jobs/train_model.py
+++ task_graph_ml_stage/src/jobs/train_model.py
@@ -95,7 +95,7 @@ def main() -> None:
     }
     print(f"Training complete. Metrics: {metrics}")
 
-    print ("Testing this works well when I commit  and deployed!!!!!!")
+    print ("Testing this works well when I commit  and deployed 2!!!!!!")
     
     # Generate version name
     version_name = f"v_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
